d25d2b6862e8d69e04c65de650795dd1af3baf60
kazi-amir <kaziah444@gmail.com>
2025-11-09 19:06:51 +0600

    Added Segmented Sieve

---
+++ code/Code/Segmented Sieve.cpp
@@ -0,0 +1,18 @@
+void segSeive(ll low, ll high) {
+    vector < bool > area((high - low) + 1, true);
+    for (ll i = 0; primes[i]*primes[i] <= high; i++) {
+        ll start = ((low / primes[i]) * primes[i]);
+        if (start < low) start += primes[i];
+        for (ll j = start; j <= high; j += primes[i]) {
+            if (j == primes[i]) continue;
+            area[j - low] = false;
+        }
+    }
+    for (ll i = 0; i < (high - low) + 1; i++) {
+        if (area[i]) {
+            if (i + low != 1 and i + low != 0) {
+                cout << i + low << endl;
+            }
+        } 
+    }   
+}
