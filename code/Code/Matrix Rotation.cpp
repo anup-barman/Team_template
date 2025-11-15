b99e94549486807ea05ee582935731fecf0c429b
kazi-amir <kaziah444@gmail.com>
2025-11-15 13:36:49 +0600

    Meaw

--- code/Code/Matrix Rotation.cpp
+++ code/Code/Matrix Rotation.cpp
@@ -7,4 +7,4 @@ now = {{ -1, 0, 2 * p}, {0, 1, 0}, {0, 0, 1}};
 //mirror with y axis at point p
 now = {{1, 0, 0}, {0, -1, 2 * p}, {0, 0, 1}};
 op[i + 1] = matMul(now, op[i]); // this
-// op[i + 1] = matMul(op[i], now); //not this
+// op[i + 1] = matMul(op[i], now); //not this 
